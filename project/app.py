020b7825d3f9de8597a551af631510ab8e760c1b
KimEvgeniya <jenda-penda@mail.ru>
2024-12-08 04:09:01 +0300

    Получение ключа по городу и обработка ошибок при использовании API AccuWeather

--- project/app.py
+++ project/app.py
@@ -3,4 +3,28 @@ import requests
 import os
 from config import API_KEY
 
+
+
 app = Flask(__name__)
+
+
+
+def get_location_key(location):
+    location_url = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete'
+    params = {
+        'apikey': API_KEY,
+        'q': location,
+        'language': 'ru-ru'
+    }
+
+    try:
+        location_response = requests.get(location_url, params=params)
+        location_response.raise_for_status()
+        location_data = location_response.json()
+        if location_data:
+            return location_data[0]['Key']
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f'Ошибка при запросе локации: {e}')
+        return None
